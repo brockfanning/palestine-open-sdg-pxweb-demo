ccb8ccd9c176e8dbe1366883782b5b9fd1b757b9
Brock Fanning <brockfanning@gmail.com>
2022-08-21 16:52:05 -0400

    Indicator alteration and series support

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,3 +1,9 @@
 from sdg.open_sdg import open_sdg_build
 
-open_sdg_build(config='config_data.yml')
+def my_indicator_alteration(indicator, context):
+    # If the number of years is 2 or less, use a bar chart.
+    if indicator.data.Year.nunique() <= 2:
+        indicator.meta['graph_type'] = 'bar'
+    return indicator
+
+open_sdg_build(config='config_data.yml', alter_indicator=my_indicator_alteration)
